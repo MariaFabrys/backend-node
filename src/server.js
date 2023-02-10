a42e7d44b31d2b0fa5c99cd85fd648c8bf3dc35f
Maria Fabris <mariaeduardafabris12@gmail.com>
2023-02-10 17:19:32 -0300

    how to require and response json file

--- src/server.js
+++ src/server.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const listaCursos = require ('./db/cursos.json')
 const app = express()
 const port = 3100
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const msg = [{nome: 'ola mundo json!'}]
+  res.json(msg)
+})
+
+app.get('/cursos', (req, res) => {
+  res.json(listaCursos)
+})
+
+
+
+app.post('/cursos', (req, res) => {
+  res.json({message: 'ola post json!'})
+})
+
+
+app.put('/cursos', (req, res) => {
+  res.send('Fiz um update no curso!')
+})
+
+
+app.all('*', (req, res) => {
+  res.send('404 Rota não encontrada')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
